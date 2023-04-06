d90b9d79c252e765d3e52fad485da9810fe66f70
Bilhalv <128099321+Bilhalv@users.noreply.github.com>
2023-04-06 11:33:18 -0300

    falta só copy function e outras macros

--- macros/magia.js
+++ macros/magia.js
@@ -1,4 +1,6 @@
-function processInput() {
+function processInput(event) {
+    event.preventDefault();
+
     const nom = document.getElementById("nom").value;
     const dur = document.getElementById("dur").value;
     const alv = document.getElementById("alv").value;
@@ -10,7 +12,7 @@ function processInput() {
 
     const macro = "&{template:spell}{{spellname=" + nom +"}}{{type="+ type +"}}{{execution="+ exe +"}}{{duration="+ dur +"}}{{range="+ alv +"}}{{targetarea="+ alv +"}}{{resistance="+ resist +"}}{{description="+ desc +"}}{{cd="+ cd +"}}";
 
-    document.getElementById("magia").innerHTML = "<article style='max-width: 50%; margin: 0 auto;'>" + macro + "</article>";
+    document.getElementById("magia").innerHTML = "<article style='max-width: 25%; margin: 0 auto; text-align: left;'>" + macro + "</article>";
 }
 
 // &{template:spell}{{spellname=NOME DA MAGIA}}{{type=TIPO}}{{execution=AÇÃO}}{{duration=DURAÇÃO}}{{range=ALCANCE}}{{targetarea=ALVO}}{{resistance=RESISTÊNCIA}}{{description=DESCRIÇÃO DA MAGIA, PODENDO SER O TEXTO DELA, ANOTAÇÕES, OU BOTÕES E GIFS}}{{cd=CD DA MAGIA}}
